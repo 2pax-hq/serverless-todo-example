aa3748df1c0cc3ef9b61bf4e45ba6bd45f336653
Christian Klotz <hello@christianklotz.co.uk>
2018-04-20 15:04:23 +0100

    Rename checked to done

--- todo/todo.go
+++ todo/todo.go
@@ -8,9 +8,10 @@ type ValidationError string
 
 func (e ValidationError) Error() string { return "validation error: " + string(e) }
 
+// Task represents a to-do item
 type Task struct {
 	ID        string    `json:"id"`
-	Checked   bool      `json:"checked"`
+	Done      bool      `json:"done"`
 	Note      string    `json:"note"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
